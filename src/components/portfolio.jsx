bb0dcd9c704ee91313ace91e4bf2ef088ad90ce9
MahmudAhmed <moe.purplefox@gmail.com>
2020-11-12 03:55:35 -0500

    initial commit

---
+++ src/components/portfolio.jsx
@@ -0,0 +1,190 @@
+import React from "react";
+
+//import stock
+import stock from "../img/image1.png";
+import stock1 from "../img/image2.png";
+import stock2 from "../img/image3.png";
+import stock3 from "../img/image4.png";
+import stock4 from "../img/image5.png";
+import stock5 from "../img/image6.png";
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  A few of my recent projects.
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="https://halal-table.herokuapp.com/"  title="View Site">
+                  <div className="work-img">
+                    <img src={stock} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Halal Tables</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Rails, PostgreSQL, JavaScript, React, Redux, AWS, REST 
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="https://crime-watch.herokuapp.com/" title="View Site">
+                  <div className="work-img">
+                    <img src={stock1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Crime Watch</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            MongoDB, ExpressJS, ReactJS, NodeJS, Google Maps API
+                          </span>{" "}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="https://mahmudahmed.github.io/Bull-E-Season/" title="View Site">
+                  <div className="work-img">
+                    <img src={stock2} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">BULL-E Season</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5, CSS3, Vanilla JavaScript, DOM
+                          </span>
+                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="https://mahmudahmed.com/TriviaUniverse/" title="View Site">
+                  <div className="work-img">
+                    <img src={stock3} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Trivia University</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            React, HTML, CSS, APIs
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="http://mahmudahmed.com/Data-Visualization-Boston-Housing-Market/" title="View Site">
+                  <div className="work-img">
+                    <img src={stock4} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Data Visualization</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">JavaScript, HTML/CSS, Canvas, D3</span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a target="_blank" href="https://expo.io/@mahmudahmed/BaitnHook" title="View Site">
+                  <div className="work-img">
+                    <img src={stock5} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Bait & Hook Mobile App</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            React-Native, Firebase, EXPO
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
